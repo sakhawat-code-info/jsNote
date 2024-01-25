bbd40ccae9f5e78f91b577dfec041320dc04450d
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-01-25 15:25:26 +0600

    add more file for read

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-console.log("checking");
+
+// array 
+
+
+let myArray = [0, 1, 2, 3, 4, 5];
+myArray.push(9);
+console.log(myArray);
+
+let newArray = myArray.join() // join korle copy hoye jay data but array hisabe  na string hisabe
+
+
+
+console.log(newArray);
+
+
+
+
+
+console.log("--------------------------");
